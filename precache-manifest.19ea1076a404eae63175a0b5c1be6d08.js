377b9a38171f27c83a235a15f27275b58befcfe5
DevLaukey <devlaukeymwaura@gmail.com>
2022-06-27 12:51:15 +0300

    Updates

---
+++ precache-manifest.19ea1076a404eae63175a0b5c1be6d08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dba574103dc17c9f208",
+    "url": "/movie-task/static/css/main.cfa2a25d.chunk.css"
+  },
+  {
+    "revision": "0dba574103dc17c9f208",
+    "url": "/movie-task/static/js/main.0dba5741.chunk.js"
+  },
+  {
+    "revision": "ad39ee43e5f1e39ec27e",
+    "url": "/movie-task/static/js/1.ad39ee43.chunk.js"
+  },
+  {
+    "revision": "92055747ff6f6b9194f3",
+    "url": "/movie-task/static/js/2.92055747.chunk.js"
+  },
+  {
+    "revision": "dc569cdbc12c92319cfc",
+    "url": "/movie-task/static/js/runtime~main.dc569cdb.js"
+  },
+  {
+    "revision": "7b81a63671ef20a3736f7181b0b40948",
+    "url": "/movie-task/index.html"
+  }
+];
